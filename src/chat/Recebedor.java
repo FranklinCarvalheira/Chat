06759b35e7e57f8dce4fcd018d3502812c4eb4d9
Franklin <franklin.carvalheira@fatec.sp.gov>
2019-11-30 18:31:25 -0300

    Projeto Final

---
+++ src/chat/Recebedor.java
@@ -0,0 +1,26 @@
+package chat;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Recebedor implements Runnable {
+    
+    private InputStream servidor;
+    
+    public Recebedor(InputStream servidor){
+        this.servidor = servidor;
+    }
+
+    @Override
+    public void run() {
+        // Recebe mensagens do servidor e imprime na tela
+        @SuppressWarnings("resource")
+		Scanner input = new Scanner(this.servidor);
+        
+        while(input.hasNext()){
+            System.out.println(input.nextLine());
+        }
+    }
+    
+    
+}
